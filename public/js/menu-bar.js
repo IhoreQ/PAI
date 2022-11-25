45931c3273f413a5af74a33cc3fc5cdf7cf438fd
IhoreQ <iggibobek@gmail.com>
2022-11-25 14:20:54 +0100

    Edited homepage

--- public/js/menu-bar.js
+++ public/js/menu-bar.js
@@ -1,5 +1,10 @@
 const buttons = document.querySelectorAll(".menu-bar-element");
 
+const menuBar = document.querySelector(".menu-bar");
+const menuBurger = document.querySelector(".menu-burger");
+const menuContainer = document.querySelector(".menu-container");
+
+
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
 
@@ -9,5 +14,26 @@ buttons.forEach((button) => {
         const containerName = button.id + "-container";
         document.querySelector(".container-is-active").classList.remove("container-is-active");
         document.querySelector("." + containerName).classList.add("container-is-active");
+
+        if (menuBar.classList.contains("menu-bar-active") === true) {
+            menuContainer.classList.remove("menu-container-active");
+            setTimeout(() => {
+                menuBar.classList.remove("menu-bar-active");
+            }, 500)
+    }
     });
+});
+
+menuBurger.addEventListener("click", () => {
+
+    if (menuBar.classList.contains("menu-bar-active") === true) {
+            menuContainer.classList.remove("menu-container-active");
+            setTimeout(() => {
+                menuBar.classList.remove("menu-bar-active");
+            }, 500)
+    }
+    else {
+        menuBar.classList.add("menu-bar-active");
+        menuContainer.classList.add("menu-container-active");
+    }
 });
